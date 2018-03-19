0bf4025288989b28c0444ca4b4bd20afe724dbe4
Sudheendra <sudhimail@gmail.com>
2018-03-19 19:54:59 +0530

    First commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,8 +8,32 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return $this->render('article/index.html.twig',['title'=>'Hello World']);
 
+    }
+
+    /**
+     * @Route("/news/{method}")
+     */
+    public function show($method)
+    {
+
+        $comments = ['Test1','Test2','Test3'];
+
+        return $this->render('article/show.html.twig',[
+            'title' => ucwords(str_replace('-', ' ',$method)),
+            'comments' => $comments,
+        ]);
+    }
 }
